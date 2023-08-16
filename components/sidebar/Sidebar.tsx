a70c1ff6f156970ace73440fea91dcb6a66e33a5
Phatcharaphon Chaisaenthao <phatcharaphon.c@kkumail.com>
2023-08-16 10:12:00 +0700

    created login button

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
-
 const SideBar = () => {
+  
   return (
     <div className='w-1/5 bg-green-800 text-center rounded-r-md border-r-2 border-black'>
         <div className='text-white text-lg mt-4'>
             Good Morning 
+        </div>
+        <div>
         </div>
         <Link href='/admin/analyticsReport' className='mt-16'>
             <button className='text-left w-[70%] rounded border-2 border-black bg-white px-2 hover:bg-slate-300'>Analytics Report</button>
@@ -22,6 +25,9 @@ const SideBar = () => {
         <Link href='/admin' className='mt-1'>
             <button className='text-left w-[70%]  rounded border-2 border-black bg-white px-2 hover:bg-slate-300'>ℹ️ GuideBook</button>
         </Link>
+        <div>
+            <button className='text-left w-[70%]  rounded border-2 border-black bg-white px-2 hover:bg-slate-300'>🔒 LogIn</button>
+        </div>
     </div>
   )
 }
