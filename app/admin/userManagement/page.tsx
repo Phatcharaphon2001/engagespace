bb5392b4199b7e38a42aa6a37e6190013bc01bb6
Phatcharaphon Chaisaenthao <phatcharaphon.c@kkumail.com>
2023-08-17 16:29:46 +0700

    create-UserTable

--- app/admin/userManagement/page.tsx
+++ app/admin/userManagement/page.tsx
@@ -1,10 +1,44 @@
 "use client"
-import React from 'react'
+import UserTable from '@/components/tables/userTable';
+import React, {useState, useEffect} from 'react'
 
-type Props = {}
 
-export default function UserMananementPage({ }: Props) {
+
+
+  interface User {
+    _id: string,
+    name: string,
+    email: string,
+    nickname: string,
+    title: string,
+    team: string,
+    picture: string,
+    mobile: string,
+    role: string,
+
+  }
+
+export default function UserMananementPage() {
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:3000/api/usermaster')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then(resJson => {
+        setUsers(resJson);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <p>Hello</p>
+    <div>
+      <UserTable users={users} />
+    </div>
   )
 }
---
+++ components/tables/userTable.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+
+interface UsersTableProps {
+  users: any,
+}
+
+
+const userTable : React.FC<UsersTableProps> = ({
+  users,
+}) => {
+  return (
+    
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Color
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+          {users?.map((user: any) => (
+             <tr key={user._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                 {user.name}
+             </th>
+             <td className="px-6 py-4">
+                 {user.team}
+             </td>
+             <td className="px-6 py-4">
+                 {user.role}
+             </td>
+             <td className="px-6 py-4">
+                 null
+             </td>
+             <td className="px-6 py-4">
+                 <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+             </td>
+         </tr>
+          ))}
+         
+        </tbody>
+    </table>
+</div>
+
+  )
+}
+
+export default userTable
