7c7895b3ef2579c092e0cfbb5eff591a95d2baae
Phatcharaphon Chaisaenthao <phatcharaphon.c@kkumail.com>
2023-08-25 08:56:30 +0700

    created mood header

--- app/admin/moodManagement/page.tsx
+++ app/admin/moodManagement/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function moodManagementPage({}: Props) {
   return (
-    <div>moodManagementPage</div>
-  )
+    <div className="ml-10 w-full">
+      <div className="my-10">moodManagementPage</div>
+      <div>
+        <p>Mood Header</p>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+        />
+      </div>
+      <div>
+        <p>Mood Title</p>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+        />
+      </div>
+    </div>
+  );
 }
